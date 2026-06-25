98e7990b2cb49baf529cac7f69ed3ce0f6bbeea8
vivan <ivanrojas1405@gmail.com>
2026-06-25 20:42:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def saludar(nombre):
+    """Función que saluda a una persona."""
+    return f"Hola, {nombre}! Bienvenido a ML."
+
+if __name__ == '__main__':
+    mensaje = saludar("Estudiante")
+    print(mensaje)
+
+# cd proyecto_1        venv\Scripts\activate      - deactivate
