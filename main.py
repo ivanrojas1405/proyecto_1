c251150d19c348d88f58c218da23606f165bbd16
vivan <ivanrojas1405@gmail.com>
2026-06-27 10:17:49 +0200

    prueba 2 el- 27-06-2026n

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     mensaje = saludar("Estudiante")
     print(mensaje)
     print("estoy en el otro staging")
-
+    print("segunda ejempplo:270626")git add .
 # cd proyecto_1        venv\Scripts\activate      - deactivate
 #
 #  ejemplo  para git .
